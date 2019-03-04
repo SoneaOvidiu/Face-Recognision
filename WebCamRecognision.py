443051e920f7cf78f6fcd2dd20ff84d981773e35
SoneaOvidiu <47274610+SoneaOvidiu@users.noreply.github.com>
2019-03-04 17:54:43 +0200

    Update WebCamRecognision.py

--- WebCamRecognision.py
+++ WebCamRecognision.py
@@ -1,15 +1,6 @@
 import face_recognition
 import cv2
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
@@ -17,18 +8,12 @@ video_capture = cv2.VideoCapture(0)
 ovidiu_image = face_recognition.load_image_file("Eu.jpg")
 ovidiu_face_encoding = face_recognition.face_encodings(ovidiu_image)[0]
 
-# Load a sample picture and learn how to recognize it.
-juli_image = face_recognition.load_image_file("Juli.jpg")
-juli_face_encoding = face_recognition.face_encodings(juli_image)[0]
-
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    ovidiu_face_encoding,
-    juli_face_encoding
+    ovidiu_face_encoding
 ]
 known_face_names = [
-    "Ovidiu",
-    "Juli"
+    "Ovidiu"
 ]
 
 # Initialize some variables
@@ -67,7 +52,6 @@ while True:
             face_names.append(name)
 
     process_this_frame = not process_this_frame
-
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
